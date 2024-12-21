cab5d61785db5e4a83660228a25be1c7ef4db0dc
Alex Langevin <alexlangev@gmail.com>
2024-12-21 00:55:20 -0500

    with special characters

--- main.go
+++ main.go
@@ -5,110 +5,29 @@ import (
 	"math/rand/v2"
 )
 
-var regChars = [52]rune{
-	'A', 
-	'B', 
-	'C', 
-	'D', 
-	'E', 
-	'F', 
-	'G', 
-	'H', 
-	'I', 
-	'J', 
-	'K', 
-	'L', 
-	'M', 
-	'N', 
-	'O', 
-	'P', 
-	'Q', 
-	'R', 
-	'S', 
-	'T', 
-	'U', 
-	'V', 
-	'W', 
-	'X', 
-	'Y', 
-	'Z',
-	'a', 
-	'b', 
-	'c', 
-	'd', 
-	'e', 
-	'f', 
-	'g', 
-	'h', 
-	'i', 
-	'j', 
-	'k', 
-	'l', 
-	'm', 
-	'n', 
-	'o', 
-	'p', 
-	'q', 
-	'r', 
-	's', 
-	't', 
-	'u', 
-	'v', 
-	'w', 
-	'x', 
-	'y', 
-	'z',
-}
-
-var specChars = [32]rune{	
-	'!', 
-	'"', 
-	'#', 
-	'$', 
-	'%', 
-	'&', 
-	'\'', 
-	'(', 
-	')', 
-	'*', 
-	'+', 
-	',', 
-	'-', 
-	'.', 
-	'/', 
-	':', 
-	';', 
-	'<', 
-	'=', 
-	'>', 
-	'?', 
-	'@', 
-	'[', 
-	'\\', 
-	']', 
-	'^', 
-	'_', 
-	'`', 
-	'{', 
-	'|', 
-	'}', 
-	'~',
-}
-
 func main() {
-	pwGen(3)
-	pwGen(19)
-	pwGen(15)
+	pwGen(3, true)
+	pwGen(19, false)
+	pwGen(15, true)
 }
 
-func pwGen (length int) string {
+func pwGen (length int, incSpecChars bool) string {
+	regChars := []rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+	specChars := []rune{'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*','+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'}
+
 	pw := ""
 
+	charSpace := make([]rune, len(regChars))
+	copy(charSpace, regChars)
+	if incSpecChars {
+		charSpace = append(charSpace, specChars...)
+	}
+
 	for i := 0; i < length; i++ {
-		pw += string(regChars[rand.IntN(len(regChars))])
+		pw += string(charSpace[rand.IntN(len(charSpace))])
 	}
 
 	fmt.Println(pw)
 	return pw
 }
-
